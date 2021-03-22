c7bcf40383a243216fdda0f1976ffa285b99952c
salomao beling <belinglima@gmail.com>
2021-03-22 00:01:10 -0300

    Updates

---
+++ precache-manifest.f216985e9107f3c6bf865ed624fc9bb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233597e5ce77aba8ef94e90c1d15dbbe",
+    "url": "/enjoy2learn/index.html"
+  },
+  {
+    "revision": "b82fb3fa5c19ae276a98",
+    "url": "/enjoy2learn/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b2090d651ad059389c95",
+    "url": "/enjoy2learn/static/js/2.618b00cb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/enjoy2learn/static/js/2.618b00cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82fb3fa5c19ae276a98",
+    "url": "/enjoy2learn/static/js/main.59d616f9.chunk.js"
+  },
+  {
+    "revision": "6bb17835d0bdf64eb7ab",
+    "url": "/enjoy2learn/static/js/runtime-main.58811b34.js"
+  }
+]);
